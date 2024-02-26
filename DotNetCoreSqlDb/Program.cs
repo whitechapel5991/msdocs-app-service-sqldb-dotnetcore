8469ed583088a6bf8dc7b1230e4de1c633365a18
Dzianis <whitechapel2995@gmail.com>
2024-02-26 22:36:13 +0100

    get rid of redis config

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -6,11 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
-});
+//builder.Services.AddStackExchangeRedisCache(options =>
+//{
+//    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+//    options.InstanceName = "SampleInstance";
+//});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
